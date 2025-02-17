c01a22c8198205543122e6ec911ac9f626b4abe7
JoyMoGas <joseantoniomontanogastelum5@gmail.com>
2025-02-16 18:31:13 -0700

    Responsive NavBar not working

---
+++ static/scrollTo.js
@@ -0,0 +1,16 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", () => {
+    const flecha = document.querySelector(".contenedor-flecha");
+    const seccionDestino = document.getElementById("seccion-destino");
+    if (flecha && seccionDestino) {
+        flecha.addEventListener("click", (event) => {
+            event.preventDefault(); // Evita el scroll automático del navegador
+            const extraOffset = 100; // Ajusta este valor para cambiar el espacio extra
+            const offsetTop = seccionDestino.getBoundingClientRect().top + window.scrollY + extraOffset;
+            window.scrollTo({
+                top: offsetTop,
+                behavior: "smooth"
+            });
+        });
+    }
+});

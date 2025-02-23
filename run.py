b7bbb2ae682d8bf566ef79765c44a52db5fa80e1
JoyMoGas <joseantoniomontanogastelum5@gmail.com>
2025-02-23 11:23:03 -0700

    Deployable First test

--- run.py
+++ run.py
@@ -67,4 +67,4 @@ def not_found_page():
     return render_template('404.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT", default=5000))

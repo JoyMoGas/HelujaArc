2d747c4a9481f89e33d72f21b90f9a82eb064be6
JoyMoGas <joseantoniomontanogastelum5@gmail.com>
2025-02-23 11:30:54 -0700

    Deployable First test V1

--- run.py
+++ run.py
@@ -67,4 +67,6 @@ def not_found_page():
     return render_template('404.html')
 
 if __name__ == '__main__':
+    if not os.path.exists(app.config["UPLOAD_FOLDER"]):
+        os.makedirs(app.config["UPLOAD_FOLDER"])    
     app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT", default=5000))
